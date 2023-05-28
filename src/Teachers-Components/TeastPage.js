58f772bca5a59f92678b7c4d89b2946900fe0ddf
Abdulwasim-S <abdulwasimsmech@gmail.com>
2023-05-28 14:16:24 +0530

    firast_commit

---
+++ src/Teachers-Components/TeastPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TeastPage = () => {
+  return (
+    <div>TeastPage</div>
+  )
+}
+
+export default TeastPage
---
+++ src/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <>
+    <h1 className='text-success mt-5'>Welcome</h1>
+    <h3 className='mt-3'>to Students and Teachers dashboard page</h3>
+    <div className='mt-3'>Please select which page do you need at the top</div>
+    </>
+  )
+}
+
+export default Welcome;
